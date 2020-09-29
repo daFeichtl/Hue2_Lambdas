2e940220a011f2063283d64f668599702cfce1c2
BuildTools <unconfigured@null.spigotmc.org>
2020-09-29 10:41:22 +0200

    created list, added samples and let them print with foreach

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HalloJavamitForEach {
+    List<String> list = new ArrayList<>();
+
+    private void printList(){
+        list.add("Hellou");
+        list.add("WASSUP");
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
 }
