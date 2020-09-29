05f990eb33e1eaa311ccd8ea82dc1ecf6e019537
BuildTools <unconfigured@null.spigotmc.org>
2020-09-29 10:47:40 +0200

    added lambda expression to printList

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -4,11 +4,16 @@ import java.util.List;
 public class HalloJavamitForEach {
     List<String> list = new ArrayList<>();
 
+    public static void main(String[] args) {
+        new HalloJavamitForEach().printList();
+    }
     private void printList(){
         list.add("Hellou");
         list.add("WASSUP");
         for (String s : list) {
             System.out.println(s);
         }
+        list.forEach((String s) -> System.out.println(s));
     }
+
 }
