f81a39ed140ba602061a57b1abefc4ce45510558
BuildTools <unconfigured@null.spigotmc.org>
2020-10-04 21:51:28 +0200

    RationalCalculator.java: impl +-*/

--- src/Number.java
+++ src/Number.java
@@ -21,22 +21,5 @@ public class Number {
     public interface CalculationOperation {
         Number calc(Number x, Number y);
     }
-    public abstract class AbstractCalculator {
-        protected CalculationOperation add;
-        protected CalculationOperation subtract;
-        protected CalculationOperation multiply;
-        protected CalculationOperation divide;
 
-        public AbstractCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
-            this.add = add;
-            this.subtract = subtract;
-            this.multiply = multiply;
-            this.divide = divide;
-        }
-        public abstract Number add(Number a, Number b);
-        public abstract Number subtract(Number a, Number b);
-        public abstract Number multiply(Number a, Number b);
-        public abstract Number divide(Number a, Number b);
-
-    }
 }
--- src/RationalCalculator.java
+++ src/RationalCalculator.java
@@ -1,2 +1,49 @@
-public class RationalCalculator {
+public class RationalCalculator extends AbstractCalculator {
+    public RationalCalculator(Number.CalculationOperation add, Number.CalculationOperation subtract, Number.CalculationOperation multiply, Number.CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        Number.CalculationOperation add = (Number x, Number y) -> {
+            Number ad = new Number();
+            ad.setA(x.getA()+y.getA());
+            ad.setB(x.getB()+y.getB());
+            return ad;
+        };
+        return add.calc(a,b);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        Number.CalculationOperation add = (Number x, Number y) -> {
+            Number su = new Number();
+            su.setA(x.getA()-y.getA());
+            su.setB(x.getB()-y.getB());
+            return su;
+        };
+        return add.calc(a,b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        Number.CalculationOperation add = (Number x, Number y) -> {
+            Number mu = new Number();
+            mu.setB(x.getB()*y.getB());
+            mu.setA(x.getA()*y.getA());
+            return mu;
+        };
+        return add.calc(a,b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        Number.CalculationOperation add = (Number x, Number y) -> {
+            Number di = new Number();
+            di.setA(x.getA()/y.getA());
+            di.setB(x.getB()/y.getB());
+            return di;
+        };
+        return add.calc(a,b);
+    }
 }
